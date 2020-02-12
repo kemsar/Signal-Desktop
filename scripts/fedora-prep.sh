d81e32ccc8be48fc3dfccc0b1ff9b246b873e4fd
Kevin Sarsen <kevinsarsen@protonmail.com>
2020-02-11 17:13:52 -0700

    fix local build rpm

--- scripts/fedora-prep.sh
+++ scripts/fedora-prep.sh
@@ -7,4 +7,4 @@ curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yu
 sudo dnf install -y yarn python27 fedora-packager gcc-c++ @development-tools
 
 yarn install
-find ./patches -type f -name '*.patch' -print0 | sort -z | xargs -t -0 -n 1 patch -N -p1 -i
+#find ./patches -type f -name '*.patch' -print0 | sort -z | xargs -t -0 -n 1 patch -N -p1 -i
